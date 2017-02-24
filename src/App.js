abe8e0fa00c32ebba5279f4305887bb9847d6b9c
shassaninia <xai.soft80@gmail.com>
2017-02-24 16:05:49 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Projects from './components/Projects';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       My App
+       <Projects/>
+      </div>
+    );
+  }
+}
+
+export default App;
