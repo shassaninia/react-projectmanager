110549ff98ab87e0bc5265482d2fa2dfabf9c764
shassaninia <xai.soft80@gmail.com>
2017-02-24 22:23:28 -0500

    state, project and projectitem

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import Projects from './components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects:[]
+    }
+  }
+
+  componentWillMount(){
+    this.setState({projects:[
+         {
+          title:'Business Website',
+          category:'Web Design'
+        },
+        {
+          title:'Social App',
+          category:'Mobile Development'
+        },
+        {
+          title:'Ecommerce Shopping Cart',
+          category:'Web Development'
+        }
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
        My App
-       <Projects/>
+       <Projects projects={this.state.projects} />
       </div>
     );
   }
